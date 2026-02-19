3d4640b6669477b2d2d5d25567417681eeac0a00
Zhenya <zhekasoliar26@gmail.com>
2026-02-19 20:16:40 +0100

    Implement main logic

--- LibraryManager/LibraryManager/Program.cs
+++ LibraryManager/LibraryManager/Program.cs
@@ -2,12 +2,71 @@
 using LibraryManager;
 
 
-  
-    
-    class Program
+
+
+class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        int choice = 0, isbn = 0;
+        Library library = new Library();
+
+        do
         {
-        
-        }
+            try
+            {
+                LibraryManagerApp.DisplayMenu();
+                Console.Write("Enter your choice: ");
+                choice = int.TryParse(Console.ReadLine(), out int result) ? result : 0;
+
+                switch (choice)
+                {
+                    case 1:
+                        var availableBooks = library.GetAllAvailableBooks();
+                        if (availableBooks.Count == 0)
+                        {
+                            Console.WriteLine("No books available");
+                            break;
+                        }
+                        foreach (var b in availableBooks)
+                        {
+                            b.DisplayInfo();
+                        }
+                        break;
+                    case 2:
+                        isbn = LibraryManagerApp.GetIsbnFromUser();
+                        library.BorrowBook(isbn);
+                        break;
+                    case 3:
+                        isbn = LibraryManagerApp.GetIsbnFromUser();
+                        library.ReturnBook(isbn);
+                        break;
+                    case 4:
+                        Book book = LibraryManagerApp.GetBookInfoFromUser();
+                        library.AddBook(book);
+                        break;
+                    case 5:
+                        LibraryManagerApp.GetIsbnFromUser();
+                        library.RemoveBook(isbn);
+                        break;
+                    case 6:
+                        string query = LibraryManagerApp.GetSearchQueryFromUser();
+                        var foundBook = library.GetBookByAuthorOrTitle(query);
+                        foundBook.DisplayInfo();
+                        break;
+                    case 0:
+                        Console.WriteLine("Exiting the application. Goodbye!");
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+
+
+        } while (choice != 0);
+
+
+    }
 }
