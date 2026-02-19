1d7dd4fdca796bf48af860efdf8652f21036660f
Zhenya7354 <154931489+Zhenya7354@users.noreply.github.com>
2026-02-19 17:48:13 +0100

    Add book class

---
+++ LibraryManager/LibraryManager/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryManager
+{
+    class Book
+    {
+        public int Isbn { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int PublicationYear { get; set; }
+        Status status = Status.Available;
+        public Book(int isbn, string title, string author, int publicationYear)
+        {
+            Isbn = isbn;
+            Title = title;
+            Author = author;
+            PublicationYear = publicationYear;
+        }
+    }
+
+    enum Status
+    {
+        Available,
+        Leased
+    }
+}
