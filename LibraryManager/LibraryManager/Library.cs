bdc6418ad134fe21852a4edab30a36fd113b1343
Zhenya7354 <154931489+Zhenya7354@users.noreply.github.com>
2026-02-19 17:48:24 +0100

    Add library class

---
+++ LibraryManager/LibraryManager/Library.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryManager
+{
+    class Library
+    {
+        private List<Book> _books;
+        public Library()
+        {
+            _books = new List<Book>();
+        }
+
+        public void AddBook(Book book)
+        {
+            _books.Add(book);
+        }
+        public void RemoveBook(int isbn)
+        {
+            var bookToDelete = _books.FirstOrDefault(b => b.Isbn == isbn) ?? 
+                throw new Exception($" Book with isbn: {isbn} was not found");
+
+               _books.Remove(bookToDelete);
+        }
+    }
+}
