634de604b0882b20251ab929148b03f42c1cf21c
Zhenya <zhekasoliar26@gmail.com>
2026-02-19 18:26:13 +0100

    Implement book searching, update adding and removing book logic

--- LibraryManager/LibraryManager/Library.cs
+++ LibraryManager/LibraryManager/Library.cs
@@ -15,13 +15,28 @@ namespace LibraryManager
         public void AddBook(Book book)
         {
             _books.Add(book);
+            FileManager.SaveToFile(_books);
         }
         public void RemoveBook(int isbn)
         {
-            var bookToDelete = _books.FirstOrDefault(b => b.Isbn == isbn) ?? 
+            var bookToDelete = _books.FirstOrDefault(b => b.Isbn == isbn) ??
                 throw new Exception($" Book with isbn: {isbn} was not found");
 
-               _books.Remove(bookToDelete);
+            _books.Remove(bookToDelete);
+            FileManager.SaveToFile(_books);
         }
+
+        public Book GetBookByAuthor(string author)
+        {
+            return _books.FirstOrDefault(b => b.Author == author) ??
+                throw new Exception($" Book with author: {author} was not found");
+        }
+
+        public Book GetBookByTitle(string title)
+        {
+            return _books.FirstOrDefault(b => b.Title == title) ??
+                throw new Exception($" Book with title: {title} was not found");
+        }
+
     }
 }
