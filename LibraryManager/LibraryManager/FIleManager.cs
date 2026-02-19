22b4fbdc8d79a7b8607f08b52b552da9fe7d5191
Zhenya7354 <154931489+Zhenya7354@users.noreply.github.com>
2026-02-19 17:59:14 +0100

    Add FileManager

---
+++ LibraryManager/LibraryManager/FIleManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace LibraryManager
+{
+    static class FileManager
+    {
+        private readonly static string _path = "library.json";
+        public static void SaveToFile(Book book)
+        {
+            string json = JsonSerializer.Serialize<Book>(book);
+            File.WriteAllText(_path,json);
+        }
+
+        public static List<Book> LoadFromFile()
+        {
+            string json = File.ReadAllText(_path);
+            return JsonSerializer.Deserialize<List<Book>>(json) ?? new List<Book>();
+        }
+    }
+}
