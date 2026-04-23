6ca846adda68f48307acde10c1f020dd32d38b0a
Zhenya7354 <154931489+Zhenya7354@users.noreply.github.com>
2026-04-23 19:04:13 +0200

    refactor: Move service registrations to aplication builder extention

--- PcBuilder/PcBuilder/Extentions/ServiceExtentions.cs
+++ PcBuilder/PcBuilder/Extentions/ServiceExtentions.cs
@@ -1,6 +1,20 @@
-﻿namespace PcBuilder.Extentions
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.EntityFrameworkCore;
+using PcBuilder.Data;
+using PcBuilder.Services;
+
+namespace PcBuilder.Extentions
 {
-    public class ServiceExtentions
+    public static class ServiceExtentions
     {
+        public static WebApplicationBuilder AddAppServices(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddScoped<JwtService>();
+            builder.Services.AddOpenApi();
+            builder.Services.AddDbContext<PcDbContext>(options =>
+                options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            return builder;
+        }
     }
 }
