0c8da576e9c0eddc6127d257ea22786096f69ebe
Zhenya <zhekasoliar26@gmail.com>
2026-04-28 15:16:04 +0200

    feat(services): Partially implement cpu service

---
+++ PcBuilder/PcBuilder/Services/CpuService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PcBuilder.Data;
+using PcBuilder.Models;
+
+namespace PcBuilder.Services
+{
+    public class CpuService(PcBuilderDbContext context)
+    {
+        private readonly PcBuilderDbContext _context = context;
+        public async Task<List<Cpu>> GetAllCpuAsync()
+        {
+            return await _context.Cpus.Include(c => c.Brand).ToListAsync();
+        }
+    }
+}
