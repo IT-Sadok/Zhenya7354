990278a8234485f8cf87754738adf8785af5eb24
Zhenya <zhekasoliar26@gmail.com>
2026-04-28 15:16:23 +0200

    feat: Add components endpoint

---
+++ PcBuilder/PcBuilder/Endpoints/ComponentEndpoints.cs
@@ -0,0 +1,21 @@
+﻿using PcBuilder.Services;
+
+namespace PcBuilder.Endpoints
+{
+    public static class ComponentEndpoints
+    {
+        public static WebApplication MapComponentEndpoints(this WebApplication webApplication)
+        {
+            var group = webApplication.MapGroup("/components");
+
+            group.MapGet("/cpu", async (CpuService cpuService) =>
+            {
+                var cpus = await cpuService.GetAllCpuAsync();
+                if (cpus is null) return Results.NotFound("Cpus not found");
+                return Results.Ok(cpus);
+            });
+
+            return webApplication;
+        }
+    }
+}
