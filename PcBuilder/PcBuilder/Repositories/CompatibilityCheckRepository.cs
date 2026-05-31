c6dbb09c3ba2978048f968eaca9b13bcaefb4b35
Zhenya <zhekasoliar26@gmail.com>
2026-05-31 14:12:53 +0200

    refactor: Correct CompatibilityCheckRepository interface implementation

--- PcBuilder/PcBuilder/Repositories/CompatibilityCheckRepository.cs
+++ PcBuilder/PcBuilder/Repositories/CompatibilityCheckRepository.cs
@@ -10,47 +10,47 @@ public class CompatibilityCheckRepository(PcDbContext context) : ICompatibilityC
     private readonly PcDbContext _context = context;
     
 
-    async Task<PcCaseEntity?> ICompatibilityCheckRepository.GetCaseByIdAsync(int caseId)
+    public async Task<PcCaseEntity?> GetCaseByIdAsync(int caseId)
     {
         return await _context.PcCase.AsNoTracking().FirstOrDefaultAsync(c => c.Id == caseId);
     }
 
-    async Task<CpuEntity?> ICompatibilityCheckRepository.GetCpuByIdAsync(int cpuId)
+    public async Task<CpuEntity?> GetCpuByIdAsync(int cpuId)
     {
         return await _context.Cpu.AsNoTracking().FirstOrDefaultAsync(c => c.Id == cpuId);
     }
 
-    async Task<CpuCoolerEntity?> ICompatibilityCheckRepository.GetCpuCoolerByIdAsync(int cpuCoolerId)
+    public async Task<CpuCoolerEntity?> GetCpuCoolerByIdAsync(int cpuCoolerId)
     {
         return await _context.CpuCooler.AsNoTracking().FirstOrDefaultAsync(c => c.Id == cpuCoolerId);
     }
 
-    async Task<GpuEntity?> ICompatibilityCheckRepository.GetGpuByIdAsync(int gpuId)
+    public async Task<GpuEntity?> GetGpuByIdAsync(int gpuId)
     {
         return await _context.Gpu.AsNoTracking().FirstOrDefaultAsync(c => c.Id == gpuId);
     }
 
-    async Task<HardDriveEntity?> ICompatibilityCheckRepository.GetHardDriveByIdAsync(int hardDriveId)
+    public async Task<HardDriveEntity?> GetHardDriveByIdAsync(int hardDriveId)
     {
         return await _context.HardDrive.AsNoTracking().FirstOrDefaultAsync(c => c.Id == hardDriveId);
     }
 
-    async Task<PcMonitorEntity?> ICompatibilityCheckRepository.GetMonitorByIdAsync(int monitorId)
+    public async Task<PcMonitorEntity?> GetMonitorByIdAsync(int monitorId)
     {
         return await _context.PcMonitor.AsNoTracking().FirstOrDefaultAsync(c => c.Id == monitorId);
     }
 
-    async Task<MotherboardEntity?> ICompatibilityCheckRepository.GetMotherboardByIdAsync(int motherboardId)
+    public async Task<MotherboardEntity?> GetMotherboardByIdAsync(int motherboardId)
     {
         return await _context.Motherboard.AsNoTracking().FirstOrDefaultAsync(c => c.Id == motherboardId);
     }
 
-    async Task<PsuEntity?> ICompatibilityCheckRepository.GetPsuByIdAsync(int psuId)
+    public async Task<PsuEntity?> GetPsuByIdAsync(int psuId)
     {
         return await _context.Psu.AsNoTracking().FirstOrDefaultAsync(c => c.Id == psuId);
     }
 
-    async Task<RamEntity?> ICompatibilityCheckRepository.GetRamByIdAsync(int ramId)
+    public async Task<RamEntity?> GetRamByIdAsync(int ramId)
     {
         return await _context.Ram.AsNoTracking().FirstOrDefaultAsync(c => c.Id == ramId);
     }
