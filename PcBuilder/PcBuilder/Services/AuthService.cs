4117aa1b1ac6bfdde17a119b9f967b5af1487ec3
Zhenya7354 <154931489+Zhenya7354@users.noreply.github.com>
2026-06-07 20:31:54 +0200

    feat: Implement Auth service

---
+++ PcBuilder/PcBuilder/Services/AuthService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using PcBuilder.Entities;
+using PcBuilder.Services.Interfaces;
+
+namespace PcBuilder.Services;
+
+public class AuthService(UserManager<UserEntity> userManager, SignInManager<UserEntity> signInManager) : IAuthService
+{
+    private readonly UserManager<UserEntity> _userManager = userManager;
+    private readonly SignInManager<UserEntity> _signInManager = signInManager;
+
+
+    public async Task<UserEntity?> FindByEmailAsync(string email)
+    {
+        return await _userManager.FindByEmailAsync(email);
+    }
+
+    public async Task<SignInResult?> CheckPasswordSignInAsync(UserEntity user, string password, bool lockoutOnFailure)
+    {
+        return await _signInManager.CheckPasswordSignInAsync(user, password, lockoutOnFailure);
+    }
+
+    public async Task<IList<string>?> GetRolesAsync(UserEntity user)
+    {
+        return await _userManager.GetRolesAsync(user);
+    }
+}
---
+++ PcBuilder/PcBuilder/Services/Interfaces/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using PcBuilder.Entities;
+
+namespace PcBuilder.Services.Interfaces;
+
+public interface IAuthService
+{
+    public Task<UserEntity?> FindByEmailAsync(string email);
+    public Task<SignInResult?> CheckPasswordSignInAsync(UserEntity user, string password, bool lockoutOnFailure);
+    public Task<IList<string>?> GetRolesAsync(UserEntity user);
+}
