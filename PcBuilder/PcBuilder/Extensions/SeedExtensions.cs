6c54931436d16fa7cebd44354a663b785214649a
Zhenya7354 <154931489+Zhenya7354@users.noreply.github.com>
2026-06-09 23:09:13 +0200

    feat: Add extension class for seeders

---
+++ PcBuilder/PcBuilder/Extensions/SeedExtensions.cs
@@ -0,0 +1,25 @@
+﻿using PcBuilder.Data.Seeding;
+using PcBuilder.Data.Seeding.Interfaces;
+using PcBuilder.Data.Seeding.Seeders;
+
+namespace PcBuilder.Extentions;
+
+public static class SeedExtensions
+{
+    public static WebApplicationBuilder AddSeedingServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddScoped<IDataSeeder, BrandSeeder>();
+        builder.Services.AddScoped<IDataSeeder, CpuCoolerSeeder>();
+        builder.Services.AddScoped<IDataSeeder, CpuSeeder>();
+        builder.Services.AddScoped<IDataSeeder, GpuSeeder>();
+        builder.Services.AddScoped<IDataSeeder, HardDriveSeeder>();
+        builder.Services.AddScoped<IDataSeeder, MotherboardSeeder>();
+        builder.Services.AddScoped<IDataSeeder, PcCaseSeeder>();
+        builder.Services.AddScoped<IDataSeeder, PcMonitorSeeder>();
+        builder.Services.AddScoped<IDataSeeder, PsuSeeder>();
+        builder.Services.AddScoped<IDataSeeder, RamSeeder>();
+        builder.Services.AddScoped<IIdentityRolesSeeder, IdentityRolesSeeder>();
+        builder.Services.AddScoped<IDbSeeder, DbSeeder>();
+        return builder;
+    }
+}
