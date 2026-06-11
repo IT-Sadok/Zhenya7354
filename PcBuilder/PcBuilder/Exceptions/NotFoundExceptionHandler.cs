bce94bbc297cf4d92f7144b92e200a020dc8460b
Zhenya7354 <154931489+Zhenya7354@users.noreply.github.com>
2026-06-11 17:47:45 +0200

    feat: Add Global, NotFound and Unauthorized exception handlers

---
+++ PcBuilder/PcBuilder/Exceptions/NotFoundExceptionHandler.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PcBuilder.Exceptions;
+
+internal sealed class NotFoundExceptionHandler(
+    IProblemDetailsService problemDetailsService,
+    ILogger<NotFoundExceptionHandler> logger) : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    {
+       if(exception is not KeyNotFoundException notFoundException)
+        {
+            return false;
+        }
+
+        logger.LogError(exception, "Error: {Message}, OcurredAt: {Time}", exception.Message, DateTime.Now);
+
+        httpContext.Response.StatusCode = StatusCodes.Status404NotFound;
+
+        return await problemDetailsService.TryWriteAsync(new ProblemDetailsContext
+        { 
+            HttpContext = httpContext,
+            Exception = exception,
+            ProblemDetails = new ProblemDetails
+            {
+                Type = exception.GetType().Name,
+                Status = httpContext.Response.StatusCode,
+                Title = "Resource not found",
+                Detail = exception.Message,
+            }
+        });
+    }
+}
---
+++ PcBuilder/PcBuilder/Exceptions/UnauthorizedAccessExceptionHandler.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PcBuilder.Exceptions;
+
+internal sealed class UnauthorizedAccessExceptionHandler(
+    IProblemDetailsService problemDetailsService,
+    ILogger<UnauthorizedAccessExceptionHandler> logger) : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    {
+        if (exception is not UnauthorizedAccessException unauthorizedAccessException)
+        {
+            return false;
+        }
+
+        logger.LogError(exception, "Error: {Message}, OcurredAt: {Time}", exception.Message, DateTime.Now);
+
+        httpContext.Response.StatusCode = StatusCodes.Status401Unauthorized;
+
+        return await problemDetailsService.TryWriteAsync(new ProblemDetailsContext
+        {
+            HttpContext = httpContext,
+            Exception = exception,
+            ProblemDetails = new ProblemDetails
+            {
+                Type = exception.GetType().Name,
+                Status = httpContext.Response.StatusCode,
+                Title = "Unauthorized",
+                Detail = exception.Message,
+            }
+        });
+    }
+}
