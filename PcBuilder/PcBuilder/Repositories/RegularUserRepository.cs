e7ce77e6e85431dd0997ee214c389a9f5962602b
Zhenya7354 <154931489+Zhenya7354@users.noreply.github.com>
2026-06-14 13:31:17 +0200

    feat(Repositories): Implement repository pattern for RegularUser entity

---
+++ PcBuilder/PcBuilder/Repositories/RegularUserRepository.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PcBuilder.Data;
+using PcBuilder.Entities;
+using PcBuilder.Repositories.Interfaces;
+
+namespace PcBuilder.Repositories;
+
+public class RegularUserRepository(PcDbContext context) : IRegularUserRepository
+{
+    private readonly PcDbContext _context = context;
+    public async Task AddRegularUserAsync(RegularUserEntity dto, CancellationToken cancellationToken)
+    {
+        await _context.AddAsync(dto, cancellationToken);
+    }
+
+    public async Task<RegularUserEntity?> GetRegularUserByIdAsync(int id, CancellationToken cancellationToken)
+    {
+        return await _context.RegularUser.Include(r => r.User).AsNoTracking().FirstOrDefaultAsync(u => u.Id == id,cancellationToken) ??
+            throw new KeyNotFoundException("Regular user not found");
+    }
+
+    public async Task<List<RegularUserEntity>> GetRegularUsersAsync(CancellationToken cancellationToken)
+    {
+        return await _context.RegularUser.Include(r => r.User).AsNoTracking().ToListAsync(cancellationToken);
+    }
+
+    public Task SaveChangesAsync(CancellationToken cancellationToken)
+    {
+        return _context.SaveChangesAsync(cancellationToken);
+    }
+}
