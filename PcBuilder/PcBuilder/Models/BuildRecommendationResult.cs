a377330438500fa82cd2b25429803522ae919075
Zhenya7354 <154931489+Zhenya7354@users.noreply.github.com>
2026-07-05 15:42:35 +0200

    feat(Models): Add BuildRecommendationResult model for Ai recommendation response

---
+++ PcBuilder/PcBuilder/Models/BuildRecommendationResult.cs
@@ -0,0 +1,8 @@
+﻿namespace PcBuilder.Models;
+
+public class BuildRecommendationResult
+{
+    public BuildRequest Build { get; set; } = new();
+    public List<string> Notes { get; set; } = [];
+    public bool IsCompleted { get; set; }
+}
