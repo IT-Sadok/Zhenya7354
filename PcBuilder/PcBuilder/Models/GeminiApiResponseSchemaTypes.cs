87ce3266ffec014eb1417791eb5195b9ca1b4469
Zhenya7354 <154931489+Zhenya7354@users.noreply.github.com>
2026-07-07 15:45:01 +0200

    feat(Models): Add GeminiApiResponseSchemaTypes

---
+++ PcBuilder/PcBuilder/Models/GeminiApiResponseSchemaTypes.cs
@@ -0,0 +1,10 @@
+﻿namespace PcBuilder.Models;
+
+public static class GeminiApiResponseSchemaTypes
+{
+    public const string Object = "OBJECT";
+    public const string String = "STRING";
+    public const string Bool = "BOOLEAN";
+    public const string Array = "ARRAY";
+    public const string Decimal = "NUMBER";
+}
