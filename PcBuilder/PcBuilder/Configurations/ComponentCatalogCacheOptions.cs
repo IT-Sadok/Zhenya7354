5ae64a2adf30b258d216c9105d249416a1d74a33
Zhenya <zhekasoliar26@gmail.com>
2026-07-20 22:30:22 +0200

    feat(Configurations): Add ComponentCatalogCacheOptions to support IOptions in ComponentCatalogCache service

---
+++ PcBuilder/PcBuilder/Configurations/ComponentCatalogCacheOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace PcBuilder.Configurations;
+
+public class CacheOptions
+{
+    public int CacheAbsoluteExpirationInMinutes { get; set; } = 30;
+    public int CacheSlidingExpirationInMinutes { get; set; } = 10;
+    public int CacheSize { get; set; } = 1;
+}
