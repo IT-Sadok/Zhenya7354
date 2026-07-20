63751a5f7d43306c54f41abb36321cddbc238d2c
Zhenya <zhekasoliar26@gmail.com>
2026-07-20 14:26:03 +0200

    feat(Mappers): Add CacheKeyToComponentTypeMapper to map cache keys to component types

---
+++ PcBuilder/PcBuilder/Mappers/CacheKeyToComponentTypeMapper.cs
@@ -0,0 +1,24 @@
+﻿using PcBuilder.Enums;
+using PcBuilder.Models;
+
+namespace PcBuilder.Mappers;
+
+public static class CacheKeyToBuildTypeMapper
+{
+    public static string GetCacheKeyForBuildComponentType(BuildComponentType componentType)
+    {
+        return componentType switch
+        {
+            BuildComponentType.Cpu => ComponentCacheKeys.CpusKey,
+            BuildComponentType.Gpu => ComponentCacheKeys.GpusKey,
+            BuildComponentType.Ram => ComponentCacheKeys.RamsKey,
+            BuildComponentType.Motherboard => ComponentCacheKeys.MotherboardsKey,
+            BuildComponentType.Psu => ComponentCacheKeys.PsusKeys,
+            BuildComponentType.PcCase => ComponentCacheKeys.PcCasesKey,
+            BuildComponentType.PcMonitor => ComponentCacheKeys.MonitorsKey,
+            BuildComponentType.HardDrive => ComponentCacheKeys.HardDrivesKey,
+            BuildComponentType.CpuCooler => ComponentCacheKeys.CpuCoolersKey,
+            _ => throw new ArgumentOutOfRangeException(nameof(componentType), componentType, null)
+        };
+    }
+}
