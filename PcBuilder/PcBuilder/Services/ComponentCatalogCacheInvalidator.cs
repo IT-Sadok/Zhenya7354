2226315e0ee632c18d061ae8c25e6c694a1315ae
Zhenya <zhekasoliar26@gmail.com>
2026-07-22 21:16:54 +0300

    refactor(Services): Extract invalidation logic from ComponentCatalogCache to ComponentCatalogCacheInvalidator

---
+++ PcBuilder/PcBuilder/Services/ComponentCatalogCacheInvalidator.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using PcBuilder.Enums;
+using PcBuilder.Mappers;
+using PcBuilder.Models;
+using PcBuilder.Services.Interfaces;
+
+namespace PcBuilder.Services;
+
+public class ComponentCatalogCacheInvalidator(IMemoryCache _cache) : IComponentCatalogCacheInvalidator
+{
+    public void InvalidateComponentCatalog()
+    {
+        foreach (var key in ComponentCacheKeys.All)
+        {
+            _cache.Remove(key);
+        }
+    }
+
+    public void InvalidateCache(BuildComponentType componentType)
+    {
+        var cacheKey = CacheKeyToBuildTypeMapper.GetCacheKeyForBuildComponentType(componentType);
+        _cache.Remove(cacheKey);
+    }
+}
