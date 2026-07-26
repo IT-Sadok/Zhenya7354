8765b8df26b181a0dc028bceb389dc526579d8e8
Zhenya <zhekasoliar26@gmail.com>
2026-07-26 21:56:42 +0300

    feat(Services): Add FrankFurterCurrencyExchangeService to convert currency for recommended builds

---
+++ PcBuilder/PcBuilder/Services/Interfaces/ICurrencyExchangeService.cs
@@ -0,0 +1,8 @@
+﻿using PcBuilder.Enums;
+
+namespace PcBuilder.Services.Interfaces;
+
+public interface ICurrencyExchangeService
+{
+    Task<decimal> ConvertAsync(decimal amount, Currency from, Currency to, CancellationToken cancellationToken);
+}
